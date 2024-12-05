83b0acd39a18e8cb5853cba15d5be0109d114e81
CBMODS <cristianberrio018@gmail.com>
2024-12-04 20:27:05 -0500

    Mejora de Diseño

--- Viajando_Hoy/src/app/events/events.component.ts
+++ Viajando_Hoy/src/app/events/events.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { MenuComponent } from '../menu/menu.component';
 
 @Component({
   selector: 'app-events',
   standalone: true,
-  imports: [],
+  imports: [MenuComponent],
   templateUrl: './events.component.html',
   styleUrl: './events.component.css'
 })
--- Viajando_Hoy/src/app/panel-user/panel-user.component.ts
+++ Viajando_Hoy/src/app/panel-user/panel-user.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
+import { MenuComponent } from '../menu/menu.component';
+
 
 @Component({
   selector: 'app-panel-user',
   standalone: true,
-  imports: [],
+  imports: [MenuComponent],
   templateUrl: './panel-user.component.html',
   styleUrl: './panel-user.component.css'
 })
