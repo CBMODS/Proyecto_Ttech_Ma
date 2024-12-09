50f97cc8ba05e2b3cb19ed0d96b6334ccc9aeac2
CBMODS <cristianberrio018@gmail.com>
2024-12-08 19:20:20 -0500

    Diseño Front-end semi terminado

--- Viajando_Hoy/src/app/map/map.component.ts
+++ Viajando_Hoy/src/app/map/map.component.ts
@@ -1,9 +1,38 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { MenuComponent } from '../menu/menu.component';
+import { GoogleMapsModule } from '@angular/google-maps';
+
 @Component({
   selector: 'app-map',
-  standalone: true, // Asegura que este componente es standalone
-  imports: [], // Solo importa RouterLink si lo necesitas para este componente
+  standalone: true,
+  imports: [
+    MenuComponent,
+    GoogleMapsModule,
+    CommonModule,
+  ],
   templateUrl: './map.component.html',
   styleUrls: ['./map.component.css'],
 })
-export class MapComponent {}
+export class MapComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit(): void {}
+
+  display: any;
+  center: google.maps.LatLngLiteral = {
+    lat: 9.3034,  // Coordenada de latitud para Sincelejo
+    lng: -75.3953 // Coordenada de longitud para Sincelejo
+  };
+  zoom = 6;
+
+  /*---------- Move Map ----------*/
+  moveMap(event: google.maps.MapMouseEvent) {
+    if (event.latLng != null) this.center = event.latLng.toJSON();
+  }
+
+  /*---------- Move ----------*/
+  move(event: google.maps.MapMouseEvent) {
+    if (event.latLng != null) this.display = event.latLng.toJSON();
+  }
+}
