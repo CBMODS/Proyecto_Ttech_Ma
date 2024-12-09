6e57559c425a8103af3aee5e3620f0db493d9b60
CBMODS <cristianberrio018@gmail.com>
2024-12-08 19:46:26 -0500

    test

--- Viajando_Hoy/src/app.routes.ts
+++ Viajando_Hoy/src/app.routes.ts
@@ -3,16 +3,17 @@ import { MapComponent } from './app/map/map.component';
 import { HomeComponent } from './app/home/home.component';
 import { FaqComponent } from './app/faq/faq.component';
 import { BlogComponent } from './app/blog/blog.component';
-import { DestinyComponent } from './app/destiny/destiny.component';
 import { SlideComponent } from './app/slide/slide.component';
 import { AccountManagerComponent } from './app/account-manager/account-manager.component';
 import { ContentComponent } from './app/content/content.component';
 import { PanelUserComponent } from './app/panel-user/panel-user.component';
 import { EventsComponent } from './app/events/events.component';
+import { AboutComponent } from './app/about/about.component';
 
 export const routes: Routes = [
   { path: 'user', component: PanelUserComponent},
   { path: 'account', component: AccountManagerComponent},
+  { path: 'about', component: AboutComponent},
   { path: 'map', component: MapComponent },
   { path: 'faq', component: FaqComponent},
   { path: 'blog', component: BlogComponent},
